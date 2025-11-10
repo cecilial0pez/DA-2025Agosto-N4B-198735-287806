62fba034c144f2db81f4831d7996084e7b4cff4a
cecilial0pez <cl287806@fi365.ort.edu.uy>
2025-11-10 13:00:30 -0300

    herenciacambiosdeLaura

--- peajes/src/main/java/da/obligatorio/peajes/modelo/Bonificacion.java
+++ peajes/src/main/java/da/obligatorio/peajes/modelo/Bonificacion.java
@@ -1,29 +1,24 @@
 package da.obligatorio.peajes.modelo;
 
-public class Bonificacion {
+import java.util.Date;
+
+//abstracta para que calcule descuento por polimorfismo
+public abstract class Bonificacion {
     public String nombre;
-    public double descuento; //porcentaje de descuento
-
-
-    
-    public double getDescuento() {
-        return descuento;
-    }
-
-
-
-    public void setDescuento(double descuento) {
-        this.descuento = descuento;
-    }
-
-
+    public double descuento; // porcentaje de descuento
 
     public Bonificacion(String nombre, double descuento) {
         this.nombre = nombre;
         this.descuento = descuento;
     }
 
-  
+    public double getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(double descuento) {
+        this.descuento = descuento;
+    }
 
     public String getNombre() {
         return nombre;
@@ -33,17 +28,24 @@ public class Bonificacion {
         this.nombre = nombre;
     }
 
-    //Metodos agregados
-    public double calcularBonificacion(){
-        if(nombre.equals("Exonerado")){
-            return descuento = 1.0;
-        } else if (nombre.equals("Frecuente")){
-            return descuento =0.5;
-        } else if (nombre.equals("Trabajador")){
-            return descuento =0.8;
-        } else {
-            return 0;
-        }
-    }
+    public abstract double calcularBonificacion();
+
+    public abstract double calcularBonificacion(Propietario propietario, Vehiculo vehiculo, Puesto puesto,
+            Date fecha);
+
+    /*
+     * Metodos agregados
+     * public double calcularBonificacion() {
+     * if (nombre.equals("Exonerado")) {
+     * return descuento = 1.0;
+     * } else if (nombre.equals("Frecuente")) {
+     * return descuento = 0.5;
+     * } else if (nombre.equals("Trabajador")) {
+     * return descuento = 0.8;
+     * } else {
+     * return 0;
+     * }
+     * }
+     */
 
 }
