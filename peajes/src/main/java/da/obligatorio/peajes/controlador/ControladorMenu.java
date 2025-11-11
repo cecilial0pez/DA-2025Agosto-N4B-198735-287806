2cd9f048ca0e104e3449019a86d7092c8e0b7c3d
cecilial0pez <cl287806@fi365.ort.edu.uy>
2025-11-10 21:11:37 -0300

    primerCambioMenu

--- peajes/src/main/java/da/obligatorio/peajes/controlador/ControladorMenu.java
+++ peajes/src/main/java/da/obligatorio/peajes/controlador/ControladorMenu.java
@@ -3,6 +3,7 @@ package da.obligatorio.peajes.controlador;
 
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,43 +12,31 @@ import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import java.util.List;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+
 
 import da.obligatorio.peajes.ConexionNavegador;
 import da.obligatorio.peajes.Respuesta;
 import da.obligatorio.peajes.modelo.Administrador;
 import da.obligatorio.peajes.modelo.Fachada;
 import da.obligatorio.peajes.modelo.PeajeException;
-import da.obligatorio.peajes.modelo.Propietario;
 import jakarta.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/menu")
+@Scope("session")
 public class ControladorMenu {
 
-    private final ObjectProvider<da.obligatorio.peajes.ConexionNavegador> conexionProvider;
 
-    @Autowired
-    public ControladorMenu(ObjectProvider<da.obligatorio.peajes.ConexionNavegador> conexionProvider) {
-        this.conexionProvider = conexionProvider;
+   private final ConexionNavegador conexionNavegador; 
+    
+    public ControladorMenu(@Autowired ConexionNavegador conexionNavegador) {
+        this.conexionNavegador = conexionNavegador;
     }
 
-    // dentro de métodos que manejan requests:
-    private void usarConexion() {
-        var conexion = conexionProvider.getIfAvailable();
-        if (conexion != null) {
-            // usar conexion (asociada a la sesión actual)
-        }
-    }
-
-   @GetMapping(value = "/registrarSSE", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @GetMapping(value = "/registrarSSE", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public SseEmitter registrarSSE() {
-        var conexion = conexionProvider.getIfAvailable();
-        if (conexion != null) {
-            conexion.conectarSSE();
-            return conexion.getConexionSSE(); 
-        }
-        return null;
+        conexionNavegador.conectarSSE();
+        return conexionNavegador.getConexionSSE(); 
        
     }
 
---
+++ peajes/src/main/java/da/obligatorio/peajes/controlador/ControladorPropietario.java
@@ -0,0 +1,10 @@
+package da.obligatorio.peajes.controlador;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Scope("session")
+public class ControladorPropietario {
+    
+}
